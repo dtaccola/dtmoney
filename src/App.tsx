2fc317b883e8fc691cc23808adefd7b5a0de70e8
dtaccola <danieltaccola@yahoo.com.br>
2022-08-02 23:45:51 -0300

    estruturação do form e posicionamento do modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import Modal  from 'react-modal';
+import Modal  from "react-modal";
 import { useState } from "react";
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 Modal.setAppElement('#root');
 
@@ -21,12 +22,10 @@ function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-      <Modal
-          isOpen={isNewTransactionModalOpen}
-          onRequestClose={handleCloseNewTransactionModal}
-      >
-          <h2>Cadastrar transação</h2>
-      </Modal>
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle />
     </>
   );
