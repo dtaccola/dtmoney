8f4cf4756f6e95a065766e8f786ba74d8f6fb79c
dtaccola <danieltaccola@yahoo.com.br>
2022-08-12 11:14:59 -0300

    context

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import Modal  from "react-modal";
 import { useState } from "react";
 import { GlobalStyle } from "./styles/global";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement('#root');
 
 function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
 
   function handleOpenNewTransactionModal() {
       setIsNewTransactionModalOpen(true);
@@ -19,15 +21,17 @@ function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
+
       <NewTransactionModal 
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
 
